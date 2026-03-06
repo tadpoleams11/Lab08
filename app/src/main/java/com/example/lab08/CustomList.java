fc86d6c6ba20f7b93ef1483c6fc6152d5c741bdc
tadpoleams11 <tadpoleams11@gmail.com>
2026-03-06 13:47:55 -0700

    passing delete test

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -22,4 +22,11 @@ public class CustomList {
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
+
+    public void deleteCity(City city) {
+        if (!cities.contains(city)) {
+            throw new IllegalArgumentException("City not found in the list");
+        }
+        cities.remove(city);
+    }
 }
